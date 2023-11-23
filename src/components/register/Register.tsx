6d7701a9cd9dea681a344d20d463552ccf1944c6
Michael Munson <michael.s.munson@outlook.com>
2023-11-23 02:39:59 -0500

    push

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -8,7 +8,10 @@ export default function Register() {
     return (
         <div
             className='col hv-center hw100 fade-in bg-grad'>
-            
+            <GiChainsaw
+                style={{fontSize:'8em'}}/>
+            <GiGardeningShears
+                style={{fontSize:'8em'}}/>
         </div>
     )
 
