781ff66f04f2b988748aaf1c8b51a91cee229e1d
Michael Munson <michael.s.munson@outlook.com>
2023-11-26 14:32:30 -0500

    push

--- src/components/arborist/arboristComponents/modalComponents/AddMarker.tsx
+++ src/components/arborist/arboristComponents/modalComponents/AddMarker.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { ModalConfig } from '../../../../types/generalTypes'
-import { Divider, Button } from '@mui/material'
+import { Divider, Button, TextField } from '@mui/material'
+import { Check } from '@mui/icons-material';
 import "../../../../css/modalComponents/add-marker.css"; 
+import Spacer from '../../../utils/Spacer';
 
 interface AddMarkerProps {
     data?: ModalConfig["data"]
@@ -11,12 +13,45 @@ export default function AddMarker({
     data
 } : AddMarkerProps
 ){
+    const [markerType, setMarkerType] = useState<Set<string>>(new Set());
+
+    function changeMarkerType(type:"chips"|"wood"){
+        setMarkerType(mt => {
+            if (mt.has(type)) mt.delete(type);
+            else mt.add(type);
+            return new Set([...mt]);  
+        }); 
+    }
+
     return (
         <div className='col h-center' style={{padding:"10px"}}>
-            <Divider className='w100'>Marker Type</Divider>
-            <div className='marker-type-row'>
-                <Button>Chips</Button>
-                <Button>Wood</Button>
+            <Divider className='w100 b'>Marker Type</Divider>
+            <div className='row h-center w100 marker-type-row m2'>
+                <Button
+                    color={markerType.has('chips')?"success":"inherit"}
+                    variant={markerType.has('chips')?"contained":"outlined"}
+                    startIcon={markerType.has('chips')?<Check/>:<></>}
+                    onClick={()=>changeMarkerType('chips')}>
+                    Chips
+                </Button>
+                <Spacer width={20}/>
+                <Button
+                    color={markerType.has('wood')?"success":"inherit"}
+                    variant={markerType.has('wood')?"contained":"outlined"}
+                    startIcon={markerType.has('wood')?<Check/>:<></>}
+                    onClick={()=>changeMarkerType('wood')}>
+                    Wood
+                </Button>
+            </div>
+            <Divider className='w100 b'>Basic Details</Divider>
+            <div className='col h-center w100 m2'>
+                <TextField 
+                    className='add-marker-text-field'
+                    label='Marker Name'/>
+                <Spacer height={15}/>
+                <TextField 
+                    className='add-marker-text-field'
+                    label='Address'/>
             </div>
         </div>
     )
--- src/components/utils/Spacer.tsx
+++ src/components/utils/Spacer.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function Spacer({height}:{height:number}) {
+export default function Spacer({
+    height=1,
+    width=1
+}:{
+    height?:number,
+    width?:number
+}){
   return (
-    <div style={{height:`${height}px`}}></div>
+    <div style={{height:`${height}px`, width:`${width}px`}}></div>
   )
 }
